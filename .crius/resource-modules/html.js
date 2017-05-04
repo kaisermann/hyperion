277eb7752bf90f66ea34e56630e80b82c20c5513
Christian Kaisermann <christian@kaisermann.me>
2017-05-04 15:00:38 -0300

    Added env relative variables to nunjucks with app.json

--- .crius/resource-modules/html.js
+++ .crius/resource-modules/html.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const {join} = require('path')
 const lazypipe = require('lazypipe')
 const gulpIf = require('gulp-if')
 const nunjucksRender = require('gulp-nunjucks-render')
@@ -8,25 +10,52 @@ const crius = require('../manifest.js')
 module.exports = {
   pipelines: {
     each: asset => {
+      const tmpAppData = JSON.parse(
+        fs.readFileSync(join(process.cwd(), 'app.json'), 'utf8')
+      )
       return lazypipe()
-        .pipe(() => gulpIf('*.njk', nunjucksRender({
-          path: crius.config.paths.source,
-        })))
-        .pipe(() => gulpIf(file => !crius.params.debug && file.path.split('.').pop() === 'html',
-          htmlmin({
-            collapseWhitespace: true,
-            minifyCSS: true,
-            minifyJS: {
-              compress: {
-                drop_console: true,
+        .pipe(() =>
+          gulpIf(
+            '*.njk',
+            nunjucksRender({
+              path: crius.config.paths.source,
+              manageEnv: function (environment) {
+                environment.addFilter('padNumber', function (num) {
+                  return num < 10 ? '0' + num : num
+                })
               },
-            },
-            processConditionalComments: true,
-            removeComments: true,
-            removeEmptyAttributes: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-          }))
+              data: {
+                app: (() =>
+                  (process.env.NODE_ENV &&
+                    tmpAppData.environments[process.env.NODE_ENV]
+                    ? Object.assign(
+                        tmpAppData,
+                        tmpAppData.environments[process.env.NODE_ENV]
+                      )
+                    : tmpAppData))(),
+              },
+            })
+          )
+        )
+        .pipe(() =>
+          gulpIf(
+            file =>
+              !crius.params.debug && file.path.split('.').pop() === 'html',
+            htmlmin({
+              collapseWhitespace: true,
+              minifyCSS: true,
+              minifyJS: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+              processConditionalComments: true,
+              removeComments: true,
+              removeEmptyAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+            })
+          )
         )
     },
   },
--- README.md
+++ README.md
@@ -4,14 +4,16 @@
 
 With hyperion you can:
 * Write HTML with [Nunjucks](https://mozilla.github.io/nunjucks/)
+  * Accessible variables within Nunjucks by using the [`app.json`](https://github.com/kaisermann/hyperion/blob/master/app.json) file. To reference a variable, use `app.variable.path...`.
+  * If a `NODE_ENV` is set, the script will look for a property inside [`environments`](https://github.com/kaisermann/hyperion/blob/master/app.json#L3) with the same name as the environment name to set the `app` object.
 * Write CSS with Stylus
     * Build your website's grid with the RolleiFLEX declarative flex helper framework. (stylus)
     * Use simplified media queries with rupture (stylus)
 * Write ES6 Javascript (with async/await support)
 * See live changes (CSS/JS/HTML) on your project with [browserSync](https://www.browsersync.io/)
 * Have your [bower](https://bower.io/) or npm packages automatically included in your assets
-  * Check the [`crius.json`](https://github.com/kaisermann/crius/blob/master/crius.json) on the **root** directory
-* Need to manage a new type of resource, like, let's say... sounds? Just define it in the [`crius.json`](https://github.com/kaisermann/crius/blob/master/crius.json) and let the magic happen!
+  * Check the [`crius.json`](https://github.com/kaisermann/hyperion/blob/master/crius.json) on the **root** directory
+* Need to manage a new type of resource, like, let's say... sounds? Just define it in the [`crius.json`](https://github.com/kaisermann/hyperion/blob/master/crius.json) and let the magic happen!
 
 ## Requirements
 
