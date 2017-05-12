1a54e7a4d8c4cd863cd5e21ddea7262de23dcab8
Christian Kaisermann <christian@kaisermann.me>
2017-05-12 15:12:29 -0300

    Added {{ obj.prop.ref.string }} capability to app.json

--- hyperion.js
+++ hyperion.js
@@ -13,22 +13,49 @@ module.exports = {
     return {
       // Default 'app' object
       app: (function () {
+        // Walks through all of an objects properties
+        const transformObj = (obj, cb) => {
+          for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+              obj[key] = typeof obj[key] === 'object'
+                ? transformObj(obj[key], cb)
+                : cb(key, obj[key])
+            }
+          }
+          return obj
+        }
+
+        // Reads a string like "{{prop1.prop2.prop3...}}" and returns the actual
+        // value of 'object['prop1']['prop2']['prop3']...'
+        const parsePropStr = (key, val) => {
+          let matched
+          if ('' + val === val && (matched = /{{\s?(\S*)\s?}}/g.exec(val))) {
+            const propRealValue = matched[1]
+              .split('.')
+              .reduce((o, i) => o[i], appData)
+            return val.replace(matched[0], propRealValue)
+          }
+          return val
+        }
+
         // Reads the 'app.json' file
-        const tmpAppData = JSON.parse(fs.readFileSync('app.json', 'utf8'))
+        let appData = JSON.parse(fs.readFileSync('app.json', 'utf8'))
 
         // If NODE_ENV is set and inside the 'app.json[environments]',
         // append its variables to the 'app' object.
-        if (
-          process.env.NODE_ENV &&
-          tmpAppData.environments[process.env.NODE_ENV]
-        ) {
-          return Object.assign(
-            tmpAppData,
-            tmpAppData.environments[process.env.NODE_ENV]
-          )
-        }
-        // If not, just return the app object
-        return tmpAppData
+        appData = Object.assign(
+          appData,
+          appData.environments[process.env.NODE_ENV]
+            ? appData.environments[process.env.NODE_ENV]
+            : {}
+        )
+        delete appData.environments
+
+        // Iterate through the object and transform all
+        // {{keyerty.etc.etc2.etc3...}} into actual properties
+        transformObj(appData, parsePropStr)
+
+        return appData
       })(),
     }
   },
