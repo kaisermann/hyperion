3abb018b70e78180898738dfe697b5d92ae7f6fd
Christian Kaisermann <christian@kaisermann.me>
2017-07-26 13:55:46 -0300

    Refactor some of hyperion internal code

--- .hyperion/data.js
+++ .hyperion/data.js
@@ -1,50 +1,44 @@
-const fs = require('fs')
+const { readFileSync } = require('fs')
+const deepExtend = require('deep-extend')
 
-const isObject = o => o && o.constructor === Object
-
-function mergeDeep (target, ...sources) {
-  if (!sources.length) {
-    return target
-  }
-  const source = sources.shift()
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) {
-          target = Object.assign(target, { [key]: {} })
-        }
-        mergeDeep(target[key], source[key])
-      } else {
-        target = Object.assign(target, { [key]: source[key] })
-      }
-    }
-  }
-
-  return mergeDeep(target, ...sources)
-}
-
-function parseAppData () {
-  // Reads a string like "{{prop1.prop2.prop3...}}" and returns the actual
-  // value of 'object['prop1']['prop2']['prop3']...'
-  const parsePropStr = (key, val) => {
+const parseAppData = () => {
+  /*
+   * Reads a string like "{{prop1.prop2.prop3...}}" and returns the actual
+   * value of 'object['prop1']['prop2']['prop3']...'
+   */
+  const parsePropStr = str => {
     let matched
 
-    if ('' + val === val && (matched = /{{\s?(\S*)\s?}}/g.exec(val))) {
-      const propRealValue = matched[1]
-        .split('.')
-        .reduce((o, i) => o[i], appData)
-      return val.replace(matched[0], propRealValue)
+    if (typeof str === 'string' && (matched = /{{\s?(\S*)\s?}}/g.exec(str))) {
+      return str.replace(
+        matched[0],
+        matched[1].split('.').reduce((o, i) => o[i], appData)
+      )
     }
-    return val
+    return str
   }
 
-  // Reads the 'app.json' file
-  let appData = JSON.parse(fs.readFileSync('app.json', 'utf8'))
+  /*
+   * Iterate through the object and transform all
+   * {{property.etc.etc2.etc3...}} into actual properties
+   */
+  const parseStringReferences = obj =>
+    Object.entries(obj).reduce((acc, [key, val]) => {
+      acc[key] =
+        val.constructor === Object
+          ? parseStringReferences(val)
+          : parsePropStr(val)
+      return acc
+    }, {})
 
-  // If NODE_ENV is set and inside the 'app.json[environments]',
-  // append its variables to the 'app' object.
-  appData = mergeDeep(
+  // Reads the 'app.json' file
+  let appData = JSON.parse(readFileSync('app.json', 'utf8'))
+
+  /*
+   * If NODE_ENV is set and inside the 'app.json[environments]',
+   * append its variables to the 'app' object.
+   */
+  appData = deepExtend(
     appData,
     appData.environments
       ? appData.environments[process.env.NODE_ENV]
@@ -54,21 +48,7 @@ function parseAppData () {
   )
   delete appData.environments
 
-  // Iterate through the object and transform all
-  // {{property.etc.etc2.etc3...}} into actual properties
-  const iterateObjProps = (obj, cb) => {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        obj[key] =
-          typeof obj[key] === 'object'
-            ? iterateObjProps(obj[key], cb)
-            : cb(key, obj[key])
-      }
-    }
-    return obj
-  }
-  appData = iterateObjProps(appData, parsePropStr)
-
+  appData = parseStringReferences(appData)
   return appData
 }
 
